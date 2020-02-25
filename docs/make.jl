b3f555cdea82e89e6336bb4c5ab7ed25baec7014
GiggleLiu <cacate0129@gmail.com>
2020-02-25 15:54:24 +0800

    fix documentation

--- docs/make.jl
+++ docs/make.jl
@@ -2,8 +2,9 @@ using Documenter, NiLang
 
 using Literate
 tutorialpath = joinpath(@__DIR__, "src/examples")
+sourcepath = joinpath(@__DIR__, "../examples")
 for jlfile in ["besselj.jl"]
-    Literate.markdown(joinpath("../examples", jlfile), tutorialpath)
+    Literate.markdown(joinpath(sourcepath, jlfile), tutorialpath)
 end
 
 makedocs(;
